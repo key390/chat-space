41ca7316bf011644a6aa8cf2de85081ea5568a7b
key390 <67782828+key390@users.noreply.github.com>
2020-07-19 15:28:24 +0900

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+lock '~> 3.1'
+set :application, 'chat-space'
+set :repo_url,  'git@github.com:key390/chat-space.git'
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/key390.pem'] 
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
